1f334f3232764ae8ef65cc9606af53c244404335
David Fish <davidfish3@gmail.com>
2025-03-25 03:09:48 -0400

    obi wan gif

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 export default function AboutPage() {
   return (
@@ -9,8 +10,17 @@ export default function AboutPage() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
+        className="flex flex-col items-center space-y-4"
       >
-        <p className="text-xl text-gray-300">Hello there</p>
+        <div className="relative w-200 h-170">
+          <Image
+            src="/obiwan.gif"
+            alt="Obi-Wan Kenobi saying Hello there"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
       </motion.div>
     </div>
   );
